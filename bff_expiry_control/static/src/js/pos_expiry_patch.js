45a26b35d3d411f46ff045356b316d5f9c3b6486
adi-purwanto <odoo.superkos@gmail.com>
2026-08-18 08:44:43 +0700

    Fix Odoo 18 PosOrderline import path in pos_expiry_patch.js

--- bff_expiry_control/static/src/js/pos_expiry_patch.js
+++ bff_expiry_control/static/src/js/pos_expiry_patch.js
@@ -1,12 +1,12 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { Orderline } from "@point_of_sale/app/store/models";
+import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
 
-patch(Orderline.prototype, {
+patch(PosOrderline.prototype, {
     get_discount() {
-        const baseDiscount = super.get_discount ? super.get_discount(...arguments) : 0;
-        const product = this.get_product();
+        const baseDiscount = super.get_discount ? super.get_discount(...arguments) : (this.discount || 0);
+        const product = this.product_id || (this.get_product ? this.get_product() : null);
 
         if (product && product.is_near_expiry && product.auto_clearance_promo) {
             const clearanceDiscount = product.clearance_discount_percent || 25.0;
